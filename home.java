a554c29cf0364a5194901ec0db79bf433081537c
Brian Frausto <bfraustoc@gmail.com>
2020-09-07 12:36:15 -0500

    2do commit

--- home.java
+++ home.java
@@ -1,9 +1,12 @@
+import javax.swing.JOptionPane;
+
 /**
  * home
  */
 public class home {
 
     public static void main(String[] args) {
-        
+        int a=3;
+        JOptionPane.showMessageDialog(null, "hola"+a);
     }
 }
