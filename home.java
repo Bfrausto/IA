a5c7b61ef0c787ceae28ea2fb227950f66a45e01
Brian Frausto <bfraustoc@gmail.com>
2020-09-07 17:37:58 -0500

    Prueba lua

--- home.java
+++ home.java
@@ -2,12 +2,14 @@ import javax.swing.JOptionPane;
 
 /**
  * home
+ * 
  */
 public class home {
 
     public static void main(String[] args) {
         int a=3;
         String A="HOLA";
+        int b=4;
         JOptionPane.showMessageDialog(null, "hola"+a+A);
     }
 }
