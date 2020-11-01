01cee8ed70dfa84da1060f022701426fe3e6bfdc
Fer Lua <mafer.20177@gmail.com>
2020-11-01 17:07:49 -0600

    chatbot1

--- Programa 6/Chatbot.py
+++ Programa 6/Chatbot.py
@@ -2,12 +2,12 @@ from nltk.chat.util import Chat, reflections
 pares = [
 
         [
-        r"(.) programacion",
+        r"(.*) programacion",
         ["programacion ? o_O",]
     ],
 
     [
-        r"mi nombre es (.)",
+        r"mi nombre es (.*)",
         ["Hola %1, como estas ?",]
     ],
      [
@@ -16,10 +16,10 @@ pares = [
     ],
     [
         r"como estas ?",
-        ["Bien, y tu?",]
+        ["Bien, y tu?","He estado mejor, y tu?"]
     ],
     [
-        r"disculpa (.)",
+        r"disculpa (.*)",
         ["No pasa nada",]
     ],
     [
@@ -27,18 +27,28 @@ pares = [
         ["Hola", "Que tal",]
     ],
     [
-        r"que (.) quieres ?",
+        r"que (.*) quieres ?",
         ["Nada gracias",]
 
     ],
     [
         r"(.*) creado ?",
-        ["Fui creado hoy",]
+        ["Fui creado hoy por Fernanda y Brian",]
     ],
 
     [
-        r"me voy|finalizar",
+        r"me voy|finalizar|bye|chao",
         ["Chao","Fue bueno hablar contigo",]
+        
+    ],
+        [
+        r"que edad tienes|cuantos años tienes",
+        ["La edad es relativa","Mmmm creo que aun no cumplo un año"]
+        
+    ],
+        [
+        r"me caes bien",
+        ["Wow, gracias","igual tu a mi"]
         
     ],
     [
