e909842b2834481cf28794354ff7dd0a57a1abdb
Brian Frausto <bfraustoc@gmail.com>
2020-11-21 22:03:54 -0600

    Programa 7 1 y 2

--- Programa 7/Algoritmosgeneticos.py
+++ Programa 7/Algoritmosgeneticos.py
@@ -4,104 +4,201 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 
 salidas = open("salida.txt", "w")
+
+
 def leer():
     archivo = open("entrada.txt", "r")
     entrada = archivo.read()
-    cont,r,g,m=0,0,0,0
+    cont, r, g, m = 0, 0, 0, 0
     for ver in entrada.split(","):
         ver = ver.rstrip()
-        if cont==0:
-            r=int(ver)
-        elif cont==1:
-            g=int(ver)
+        if cont == 0:
+            r = int(ver)
+        elif cont == 1:
+            g = int(ver)
         else:
-            m=int(ver)
-        cont+=1
+            m = int(ver)
+        cont += 1
     return r, g, m
 
+
 def crear(rango):
-    individuos=[]
+    individuos = []
     for x in range(rango):
         individuos.append(random.randint(-255, 255))
-        #print(x)
+        # print(x)
     return individuos
-    
-def ruleta(individuos,generacion,rango):
-    for numGen in range(generacion):
-        sFit=0
-        funcFit=[]
-        pS=[]
-        data=[]
-        nums=[]
-        copia=[]
-        for i in individuos:
-            aux=funcionRuleta(i)#evalua x en la funcion fitness
-            funcFit.append(aux)
-            sFit+=(aux)
 
-        print(sFit)
+
+def ruleta(individuos, generacion, rango):
+    max = []
+    generaciones = []
+    for i in range(1, generacion+1):  # obtine los i de los datos
+        generaciones.append(i)
+    for numGen in range(generacion):
+        sFit = 0
+        funcFit = []
+        pS = []
+        data = []
+        nums = []
+        copia = []
+        numMax = 0
+        for x in individuos:
+
+            aux = funcionRuleta(x)  # evalua x en la funcion fitness
+            funcFit.append(aux)
+            sFit += (aux)
+       # print(sFit)
         # print("")
-        for i in funcFit:#realiza ps(i)
-            aux2=i/sFit*100
-            pS.append(aux2) 
+        for i in funcFit:  # realiza ps(i)
+            aux2 = i/sFit*100
+            pS.append(aux2)
         #print(' '.join(map(str, pS)))
-        for i in range(1,len(individuos)+1):#obtine los i de los datos
+        for i in range(1, len(individuos)+1):  # obtine los i de los datos
             nums.append(i)
 
-        for x in range(len(individuos)):#agrega los datos de la tabla a data
-            data.append([individuos[x],funcionRuleta(individuos[x]),funcFit[x],pS[x]])
-
-        headers=[ "x", "f(x)","fitness", "ps(i)"]
+        # agrega los datos de la tabla a data y saca el maximo
+        for x in range(len(individuos)):
+            if funcionRuleta(individuos[x]) > numMax:
+                numMax = funcionRuleta(individuos[x])
+            data.append([individuos[x], funcionRuleta(
+                individuos[x]), funcFit[x], pS[x]])
+        max.append(numMax)
+        headers = ["x", "f(x)", "fitness", "ps(i)"]
         salidas.write("Generacion numero : "+str(numGen)+"\n")
-        salidas.write(pandas.DataFrame(data, nums,headers).to_string())
+        salidas.write(pandas.DataFrame(data, nums, headers).to_string())
+        elegirRandom(individuos) if generacion-1 != numGen else ""
         salidas.write("\n----------------------------------\n\n")
 
-        print(pandas.DataFrame(data, nums,headers))#realiza la tabla
-        for x in range(len(individuos)):# se eliminan los datos <0.25
-           if pS[x]>=0.25:
+        # print(pandas.DataFrame(data, nums,headers))#realiza la tabla
+        for x in range(len(individuos)):  # se eliminan los datos <0.25
+            if pS[x] >= 0.25:
                 copia.append(individuos[x])
-        individuos=copia
-        copia=[]
-        if numGen==generacion-1:#realiza la grafica
-            datos = {'i':nums,
-                'ps(i)':pS}
-            df = DataFrame(datos,columns=['i','ps(i)'])
-            df.plot(x ='i', y='ps(i)', kind = 'line')
+        individuos = copia
+        copia = []
+
+        if numGen == generacion-1:  # realiza la grafica
+            print(' '.join(map(str, max)))
+            print(' '.join(map(str, generaciones)))
+            datos = {'Generacion': generaciones, 'f(x)': max}
+            df = DataFrame(datos, columns=['Generacion', 'f(x)'])
+            df.plot(x='Generacion', y='f(x)', kind='line')
             plt.show()
-            
+
+
+def elegirRandom(individuos):
+    numMut = random.randint(1, len(individuos))
+    salidas.write("\n\nOcurrieron "+str(numMut)+" mutacion/es")
+    for i in range(0, numMut):
+        numero = random.randint(0, len(individuos)-1)
+        individuos[numero] = mutacion(individuos[numero])
+
 
 def funcionRuleta(x):
-    x=(2*(x*x))-x+1
+    x = (2*(x*x))-x+1
     return x
 
-def torneo(individuos,generacion):
-    print ("hola")
 
-def crucecs(individuos,generacion):
-    print ("hola")
+def torneo(individuos, generacion, rango):
+    if len(individuos) % 2 == 0:
+        ganadores = []
+        data = []
+        max = []
+        numMax = 0
+        generaciones = []
+        cont = rango-1
+        for i in range(1, generacion+1):  # obtine los i de los datos
+            generaciones.append(i)
+        for numGen in range(generacion):
+            cont = rango-1
+            nums = []
+            data = []
+            for x in range(int(rango/2)):
+               # print(x)
+
+                if funcionTorneo(individuos[x]) > funcionTorneo(individuos[cont]):
+                    ganadores.append(individuos[x])
+                    ganadores.append(individuos[x])
+                else:
+                    ganadores.append(individuos[cont])
+                    ganadores.append(individuos[cont])
+                cont -= 1
+            # print(' '.join(map(str, individuos)))
+            # print(' '.join(map(str, ganadores)))
+            for i in range(1, len(individuos)+1):  # obtine los i de los datos
+                nums.append(i)
+            # agrega los datos de la tabla a data y saca el maximo
+            for x in range(len(individuos)):
+                if funcionTorneo(individuos[x]) > numMax:
+                    numMax = funcionTorneo(individuos[x])
+                data.append([individuos[x], funcionTorneo(individuos[x])])
+            max.append(numMax)
+            #print(' '.join(map(str, nums)))
+            if individuos == ganadores:
+                for i in range(2):
+                    rand = random.randint(0, len(ganadores)-1)
+                    ganadores[rand] = mutacion(ganadores[rand])
+
+            headers = ["valor de x", "f(x)"]
+            salidas.write("Generacion numero : "+str(numGen)+"\n")
+            salidas.write(pandas.DataFrame(data, nums, headers).to_string())
+            #elegirRandom(individuos) if generacion-1!= numGen else ""
+            salidas.write("\n----------------------------------\n\n")
+
+            individuos = ganadores
+            ganadores = []
+
+            if numGen == generacion-1:  # realiza la grafica
+                datos = {'Generacion': generaciones, 'f(x)': max}
+                df = DataFrame(datos, columns=['Generacion', 'f(x)'])
+                df.plot(x='Generacion', y='f(x)', kind='line')
+                plt.show()
+
+    else:
+        print("El numero de individuos ingresdo no es correcto. \n(recuerda que deben ser pares)")
 
 
-def crucess(individuos,generacion):
-    print ("hola")
-rango, generacion, metodo= leer()
-individuos=crear(rango)
-ruleta(individuos,generacion,rango) if metodo ==0 else torneo(individuos,generacion) if metodo ==1 else crucecs(individuos,generacion) if metodo ==2 else crucess(individuos,generacion)
+def funcionTorneo(x):
+    x = (2*(x*x))+x-2
+    return x
 
-#print(' '.join(map(str, individuos)))
-# from pandas import DataFrame
-# import matplotlib.pyplot as plt
-   
-# Data = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-#         'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-#        }
-  
-# df = DataFrame(Data,columns=['Year','Unemployment_Rate'])
-# df.plot(x ='Year', y='Unemployment_Rate', kind = 'line')
-# plt.show()
 
-# regla = bin(regla)
-#     reglas = ""
-#     for a in range(0, 10-len(regla)):
-#         reglas += '0'
-#     for x in range(2, len(regla)):
-#         reglas += regla[x]
+def mutacion(numero):
+    numBind = bin(numero)
+    mutacion, nuevoBin, mutado = "", "", 0
+    numRandom = random.randint(1, 8)
+    mutacion += "1" if numBind[0] == "-" else "0"
+
+    for a in range(0, 10-len(numBind)):
+        mutacion += '0'
+    signo = 2 if mutacion[0] == "0" else 3
+    for x in range(signo, len(numBind)):
+        mutacion += numBind[x]
+
+    numBin = mutacion
+    for i in range(0, len(mutacion)):
+        if i == numRandom:
+            nuevoBin += "1" if mutacion[i] == "0" else "0"
+        else:
+            nuevoBin += mutacion[i]
+
+    mutado = int(str(nuevoBin), 2)
+    salidas.write("\n----------------------------------\n")
+
+    salidas.write(str(numero)+" = "+numBin+" --> " +
+                  str(mutado)+" = "+nuevoBin+"\n")
+    return(mutado)
+
+
+def crucecs(individuos, generacion):
+    print("hola")
+
+
+def crucess(individuos, generacion):
+    print("hola")
+
+
+rango, generacion, metodo = leer()
+individuos = crear(rango)
+ruleta(individuos, generacion, rango) if metodo == 0 else torneo(individuos, generacion,
+                                                                 rango) if metodo == 1 else crucecs(individuos, generacion) if metodo == 2 else crucess(individuos, generacion)
--- Programa 7/tempCodeRunnerFile.py
+++ Programa 7/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-x
+def torneo(individuos,generacion,rango):
+#     if len(individuos)%2 == 0:
+#         for x 
+#     else :
+#         print("El numero de individuos ingresdo no es correcto. \n(recuerda que deben ser pares)")
+        
